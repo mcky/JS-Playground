bae98f779658240bc00ede7a6e50df30cbefc12d
Ross Mackay <ross@mcky.me>
2015-04-28 02:33:04 +0100

    Updated gulpfile and package

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,38 @@
 var gulp = require('gulp')
-	,browserSync = require('browser-sync')
+	, browserSync = require('browser-sync')
+	, browserify = require('browserify')
+	, watchify = require('watchify')
+	, source = require('vinyl-source-stream')
+	, buffer = require('vinyl-buffer')
+	, reload = browserSync.reload
 
 
-gulp.task('browser-sync', function() {
-    browserSync({
-        server: {
-            baseDir: "./app/"
-        }
-    })
+gulp.task('serve', function() {
+	browserSync({
+		server: {
+			baseDir: "./app/"
+		}
+	})
+
+	gulp.watch(['./app/js/**/*.js'], ['scripts'])
+	gulp.watch("./app/**/*.{html,css}").on('change', reload)
 })
 
-gulp.task('browserSync', function () {
-    return gulp.src("./app/**/*.{html,css,js}")
-        .pipe(browserSync.reload({stream:true}));
-});
+var bundler = browserify({
+	entries:      ['./app/js/app.js'],
+	cache:        {},
+	packageCache: {},
+	fullPaths:    true
+})
+var watcher = watchify(bundler)
 
-gulp.task('default', ['browser-sync'], function () {
-    gulp.watch("./app/**/*.{html,css,js}", ['browserSync']);
-});
+gulp.task('scripts', function() {
+	watcher
+		.bundle()
+		.pipe(source('bundle.js'))
+		.pipe(buffer())
+		.pipe(gulp.dest('./app/js'))
+		.pipe(reload({stream: true}))
+})
+
+gulp.task('default', ['scripts', 'serve'])
